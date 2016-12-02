ef6196527f4dec75b0d69d8021cdd621be235a97
j5r5myk <j5r5myk@gmail.com>
2016-12-01 19:18:11 -0500

    Add jQuery and move javascript to external file

---
+++ synth.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    alert("javascript!")
+    try {
+        context = new AudioContext();
+    }
+    catch(e) {
+        alert('Web Audio API is not supported in this browser. Use chrome or safari, maybe opera or firefox');
+    }
+
+    document.getElementById("start1").addEventListener("click", start);
+    document.getElementById("stop1").addEventListener("click", stop);
+    document.getElementById("lowpass").addEventListener("click", lowpass);
+    document.getElementById("quiet").addEventListener("click", quiet);
+
+    init();
+
+    function init() {
+    
+        oscillator = context.createOscillator();
+        gainNode = context.createGain();
+        filter = context.createBiquadFilter();
+    
+        oscillator.frequency.value = 220;
+        oscillator.type = "triangle";
+        filter.type = (typeof filter.type === 'string') ? 'lowpass' : 0; // LOWPASS
+        filter.frequency.value = 20000;
+        oscillator.connect(filter);
+        filter.connect(gainNode);
+        gainNode.connect(context.destination);
+    
+    }
+
+    function start() {
+        gainNode.gain.value = 1;
+        oscillator.start(0);
+    }
+
+    function stop() {
+        gainNode.gain.value = 0.0;
+    }
+
+    function lowpass() {
+        filter.frequency.value = 500;    
+    
+    }
+
+    function quiet() {
+        gainNode.gain.value = 0.5;
+    }
+
+
+
+    changevolume = function(element) {
+        var volume = element.value;
+        var fraction = parseInt(element.value) / parseInt(element.max);
+        // x^2 curve
+        gainNode.gain.value = fraction * fraction;
+    };
+
+    changewave = function(element) {
+        var wave = element.value;
+        oscillator.type=wave;
+    }
+
+    changepitch = function(element) {
+        var freq = element.value;
+        oscillator.frequency.value=freq;
+    }
+
+    changefilterfreq = function(element) {
+        var freq = element.value;
+        filter.frequency.value=freq;
+    }
+
+    changeFilterRes = function(element) {
+        var freq = element.value;
+        filter.Q.value=freq;
+    }
+});
